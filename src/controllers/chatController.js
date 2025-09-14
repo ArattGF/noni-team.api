0a65e0398f1283c47a02f6da9b1fa72f2c65d313
ArattGF <arattgf@gmail.com>
2025-09-13 22:54:38 -0600

    update postwebhook

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-  const VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
+const VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
 
 let test = (req, res) => {
   return res.send("hello");
@@ -40,10 +40,14 @@ let postWebhook = (req, res) => {
     // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function (entry) {
 
-      // Get the webhook event. entry.messaging is an array, but 
-      // will only ever contain one event, so we get index 0
+
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
 
     });
 
