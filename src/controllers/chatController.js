413655f9e104fca0af92f2893dcf4d38326c7cd7
ArattGF <arattgf@gmail.com>
2025-09-16 21:04:50 -0600

    fix handle postback

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -160,31 +160,6 @@ function handlePostback(sender_psid, received_postback) {
 
   callSendAPI(sender_psid, response);
 }
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-let response;
-
-  // Get the payload for the postback
-  let payload = received_postback.payload;
-
-  // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { 'text': 'Thanks!' };
-  } else if (payload === 'no') {
-    response = { 'text': 'Oops, try sending another image.' };
-  }
-
-  if (payload === 'see_products') {
-    response = { 'text': 'You can see our products here: https://example.com/products' };
-  }
-  if (payload === 'about_us') {
-    response = { 'text': 'We are a company that values quality and customer satisfaction. Learn more about us at https://example.com/about' };
-  }
-
-  // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
-}
-
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
 
